a842fd966e2038745104fc7364522a5391986890
suprith-1 <jukantisuprith@gmail.com>
2025-02-08 19:37:21 +0530

    update

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.get('/api/haha', (req, res) => {
+app.get('/haha', (req, res) => {
     res.json({ greet: "namaste" });
 });
+
+app.get('/',(req,res)=>{
+    res.send("hello");
+})
 
 const PORT = process.env.PORT || 3300;
 app.listen(PORT, () => {
